8d9bc14b2d7db70302673f5e069f99c7daa16cb2
avshetty1980 <avshetty1980@gmail.com>
2021-01-15 14:23:31 +0000

    step1 highlighting of hours based on API not done

--- decisions.md
+++ decisions.md
@@ -2,4 +2,11 @@
 
 ## packages installed:
 
+- minimal external libraries used
 - Axios
+
+## assumptions and limitations
+
+- dates are hard-coded
+- manipulate the DOM directly to highlight the active days, could be better with a library
+- the date starts at 11th and goes to 17th, but once the date goes beyond that the calendar is no longer in sync with real world calendar.
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,37 +6,46 @@ import "./Calendar.css"
 const Calendar = () => {
 
     const [dates, setDates] = useState([])
+    const [showButtonId, setShowButtonId] = useState(null)
 
     useEffect(() => {
         
         const fetchDates = async () => {
           const { data } = await axios.get("/availability")
 
-          setDates(data)
-        }
+          setDates(data)            
+        }          
 
         fetchDates()
     },[])
 
     const daysOfWeek = ["Mon","Tue", "Wed", "Thu", "Fri","Sat", "Sun"]
+    const hoursOfDay = ["08:00", "09:00", "10:00", "11:00", "12.00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00"] 
     let startNum = 10
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        setShowButtonId(event.target.id)   }
+
     return (
-        <div className="container">
-            
+        <div className="container">            
 
             <section className="timezone">Your timezone: GMT +00:00</section>
 
             <div className = "day-container">
                 {daysOfWeek.map((dayOfWeek) => {
+                    
                     return (
-                    <div 
-                    key={dayOfWeek}
-                    className={`day-item ${dayOfWeek.toLowerCase()}`}
-                    >
-                        {`${startNum+=1}th`}
-                        <br/>
-                        {dayOfWeek}
-                    </div>
+                        <button 
+                        key={dayOfWeek}
+                        className={`day-item ${dayOfWeek.toLowerCase()}`}
+                        id={dayOfWeek.toLowerCase()}
+                        onClick={(event) => handleClick(event)}
+                        >
+                            {`${startNum+=1}th`}
+                            <br/>
+                            {dayOfWeek}
+                        </button>
                     )
 
                 })}                
@@ -44,45 +53,42 @@ const Calendar = () => {
             </div>
 
             <div className = "hour-container">
-                <button className="hour-item">08:00</button>
-                <button className="hour-item">09:00</button>
-                <button className="hour-item">10:00</button>
-                <button className="hour-item">11:00</button>
-                <button className="hour-item">12:00</button>
-                <button className="hour-item">13:00</button>
-                <button className="hour-item">14:00</button>
-                <button className="hour-item">15:00</button>
-                <button className="hour-item">16:00</button>
-                <button className="hour-item">17:00</button>
-                <button className="hour-item">18:00</button>
-                <button className="hour-item">19:00</button>
-                <button className="hour-item">20:00</button>
-                <button className="hour-item">21:00</button>            
+                {hoursOfDay.map((hourOfDay) => {
+                    return (
+                        <button
+                         key={hourOfDay}
+                         className={`hour-item ${hourOfDay}`}
+                        >
+                            {hourOfDay}
+                        </button>
+                    )
+                })}
+                         
             </div>
 
             {dates.map((date) => {
                 
-            
-                // const dateRes = DateTime.fromFormat(date.date, "dd/MM/yyyy").day
-                // console.log("Date:",dateRes)
+                
 
-                const weekDay = DateTime.fromFormat(date.date, "dd/MM/yyyy").weekdayShort
-                // console.log("weekday:",weekDay)
+               const weekDay = DateTime.fromFormat(date.date, "dd/MM/yyyy").weekdayShort          
 
-                // const startTime = date.availableSlots[0].startTime
-                // console.log("startTime:",startTime)
+                
 
-                // const dateResAdd = DateTime.fromFormat(date.date, "dd/MM/yyyy").plus({ days: 4 }).day
-                // console.log("Added Date:",dateResAdd)
-
-                // const dateResMin = DateTime.fromFormat(date.date, "dd/MM/yyyy").plus({ days: -4 }).day
-                // console.log("Minused Date:",dateResMin)
+                // if(weekDay.toLowerCase() === showButtonId ) {
+                //     const slots = date.availableSlots.map(slot => {
+                //         const objValues = Object.values(slot).map(div => {
+                //             const hourEl = document.querySelector(`.${div}`)
+                //             console.log("hourEl",hourEl)
+                //         })
+                //         console.log("slots",objValues)
+                        
+                //     })
+                //     console.log("you clicked end", DateTime.fromFormat(date.date, "dd/MM/yyyy").day )
+                // }                
 
                 const el = document.querySelector(`.${weekDay.toLowerCase()} `)
                 
-                console.log("el:", el.className + "after")
-
-                return (
+                return (                   
 
                     el.classList.add("after")
                 )
